dad0d8aefe1485dfc6473a3326cae0d7394db405
Arisley Cardoso da Silva <148290892+Arisleycds@users.noreply.github.com>
2025-09-06 04:06:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,16 +9,13 @@ $path = trim(parse_url($request_uri, PHP_URL_PATH), '/');
 // Exemplo de roteamento manual
 switch ($path) {
     case '': // Rota para a página inicial
-        require 'views/home.php';
+        require 'public/home.php';
         break;
     case 'sobre-nos': // Rota para a página "Sobre Nós"
-        require 'views/sobre.php';
+        require 'public/sobre.php';
         break;
     case 'contato': // Rota para a página de contato
-        require 'views/contato.php';
-        break;
-    case 'produtos/eletronicos': // Rota mais complexa
-        require 'views/produtos-eletronicos.php';
+        require 'public/contato.php';
         break;
     default: // Se a rota não for encontrada
         http_response_code(404);
